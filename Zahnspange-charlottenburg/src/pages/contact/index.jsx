af32e1b4c7578c9d7685581820590b625a5cecc0
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-04-21 15:28:19 +0200

    change contact placeholder ihr to ihre

--- Zahnspange-charlottenburg/src/pages/contact/index.jsx
+++ Zahnspange-charlottenburg/src/pages/contact/index.jsx
@@ -100,7 +100,7 @@ const Index = () => {
                 type="email"
                 name="email"
                 id="email"
-                placeholder="Ihr Email addresse"
+                placeholder="Ihre Email addresse"
                 required
                 onChange={handleChange}
               ></input>
@@ -116,7 +116,7 @@ const Index = () => {
                 type="number"
                 name="phone"
                 id="phone"
-                placeholder="Ihr Telefonnummer"
+                placeholder="Ihre Telefonnummer"
                 onChange={handleChange}
               ></input>
             </div>
@@ -130,7 +130,7 @@ const Index = () => {
                 className="input-name"
                 name="message"
                 id="message"
-                placeholder="Ihr Nachricht"
+                placeholder="Ihre Nachricht"
                 onChange={handleChange}
               ></textarea>
             </div>
