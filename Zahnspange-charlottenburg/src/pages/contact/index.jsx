1718639fd77d216d3821d64db81d9ab7eaa277e5
Yanal-Sabbagh07 <61740352+Yanal-Sabbagh07@users.noreply.github.com>
2023-04-22 07:37:07 +0200

    change api to https

--- Zahnspange-charlottenburg/src/pages/contact/index.jsx
+++ Zahnspange-charlottenburg/src/pages/contact/index.jsx
@@ -22,7 +22,7 @@ const Index = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     axios
-      .post("http://45.9.191.215:8800/contact", {
+      .post("https://45.9.191.215:8800/contact", {
         data: state,
       })
       .then((response) => {
